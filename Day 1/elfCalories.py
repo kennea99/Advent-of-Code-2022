90313f765d2e323d4a5cae473f1a96dabe9da9c5
Alex Kennedy <kennea10@tcd.ie>
2022-12-02 01:17:01 +0000

    Added Day One Challenge

---
+++ Day 1/elfCalories.py
@@ -0,0 +1,40 @@
+
+
+
+
+
+def main():
+    sumCalories = []
+
+    elf = []
+
+    with open('input.txt', 'r') as file:
+        for line in file:
+            if line != '\n':
+                elf.append(int(line.strip()))
+
+
+            else:
+                ans = sum(elf)
+                sumCalories.append(ans)
+                elf = []
+
+    # PART 1. Getting the elf with the most calories
+    # print(max(sumCalories))
+
+
+    # PART 2. Getting the top three elves with the most calories and their sum.
+    topThree = []
+    for i in range(0, 3):
+        idx = sumCalories.index(max(sumCalories))
+        highest = max(sumCalories)
+        topThree.append(highest)
+
+        sumCalories.pop(idx)
+
+    print(sum(topThree))
+
+
+
+if __name__ == '__main__':
+    main()
