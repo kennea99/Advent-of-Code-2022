b4f51ac805da1c8f6b787cfed5addb5c0d87cca1
Alex Kennedy <kennea10@tcd.ie>
2022-12-03 19:53:52 +0000

    completed day 3

---
+++ Day 3/rucksackReorganisation.py
@@ -0,0 +1,60 @@
+import math
+
+def partOne(priorities):
+
+    with open('input.txt', 'r') as file:
+        for line in file:
+            currentLine = line.strip()
+            l = len(currentLine)-1
+            compartment1 = currentLine[:math.ceil(l/2)]
+            compartment2 = currentLine[math.ceil(l/2):]
+            for ch in compartment1:
+                if ch in compartment2:
+                    if ch.islower():
+                        priorities.append(ord(ch) - 96)
+                        break
+                    
+                    else:
+                        priorities.append(ord(ch) - 38)
+                        break
+
+    print(f'Total for Part 1: {sum(priorities)}')
+
+def partTwo(priorities):
+    badges = []
+
+
+    with open('input.txt', 'r') as file:
+        for line in file:
+            badges.append(line.strip())
+
+        for i in range(0, len(badges), 3):
+            for ch in badges[i]:
+                if ch in badges[i+1] and ch in badges[i+2]:
+                    if ch.islower():
+                        priorities.append(ord(ch) - 96)
+                        break
+                    
+                    else:
+                        priorities.append(ord(ch) - 38)
+                        break
+                    
+        print(f'Total for Part 2: {sum(priorities)}')
+
+
+def main():
+    prioritiesPart1 = []
+    partOne(prioritiesPart1)
+
+    prioritiesPart2 = []
+    partTwo(prioritiesPart2)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
